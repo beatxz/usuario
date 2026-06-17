74c5b6fc95ff60695b4d5911778538069341674d
bea <164930551+beatxz@users.noreply.github.com>
2026-06-17 11:19:34 -0300

    adicionado método que salva usuario

--- src/main/java/com/projeto/usuario/infrastructure/entity/Endereco.java
+++ src/main/java/com/projeto/usuario/infrastructure/entity/Endereco.java
@@ -1,16 +1,14 @@
 package com.projeto.usuario.infrastructure.entity;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
 @Entity
+@Builder
 @Table(name = "enderecos")
 public class Endereco {
 
