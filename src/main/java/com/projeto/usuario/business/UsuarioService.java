bc4f24844688f91be1001bf742e23ae205393dc6
bea <164930551+beatxz@users.noreply.github.com>
2026-06-22 07:38:40 -0300

    inclusão de metodo put de usuario

--- src/main/java/com/projeto/usuario/business/UsuarioService.java
+++ src/main/java/com/projeto/usuario/business/UsuarioService.java
@@ -4,7 +4,9 @@ import com.projeto.usuario.business.converter.UsuarioConverter;
 import com.projeto.usuario.business.dto.UsuarioDTO;
 import com.projeto.usuario.infrastructure.entity.Usuario;
 import com.projeto.usuario.infrastructure.exception.ConflictException;
+import com.projeto.usuario.infrastructure.exception.ResourceNotFondException;
 import com.projeto.usuario.infrastructure.repository.UsuarioRepository;
+import com.projeto.usuario.infrastructure.security.JwtUtil;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -17,6 +19,7 @@ public class UsuarioService {
     private final UsuarioRepository usuarioRepository;
     private final UsuarioConverter usuarioConverter;
     private final PasswordEncoder passwordEncoder;
+    private final JwtUtil jwtUtil;
 
     public UsuarioDTO salvaUsuario(UsuarioDTO usuarioDTO){
         emailExiste(usuarioDTO.getEmail());
@@ -44,5 +47,15 @@ public class UsuarioService {
     }
     public void deleteUsuarioPorEmail(String email){
         usuarioRepository.deleteByEmail(email);
+    }
+
+    public UsuarioDTO atualizaDadosUsuario(String token, UsuarioDTO dto) {
+        String email = jwtUtil.extrairEmailToken(token.substring(7));
+        dto.setSenha(dto.getSenha() != null ? passwordEncoder.encode(dto.getSenha()) : null);
+        Usuario usuarioEntity = usuarioRepository.findByEmail(email).orElseThrow(() ->
+                new ResourceNotFondException("Email não localizado"));
+        Usuario usuario = usuarioConverter.updateUsuario(dto, usuarioEntity);
+        return usuarioConverter.paraUsuarioDTO(usuarioRepository.save(usuario));
+
     }
 }
--- src/main/java/com/projeto/usuario/controller/UsuarioController.java
+++ src/main/java/com/projeto/usuario/controller/UsuarioController.java
@@ -41,4 +41,9 @@ public class UsuarioController {
         usuarioService.deleteUsuarioPorEmail(email);
         return ResponseEntity.ok().build();
     }
+    @PutMapping
+    public ResponseEntity<UsuarioDTO>atualizarDadoUsuario(@RequestBody UsuarioDTO dto,
+                                                          @RequestHeader ("Authorization") String token){
+        return ResponseEntity.ok(usuarioService.atualizaDadosUsuario(token,dto));
+    }
 }
